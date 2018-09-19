e813c7d883b52c71a17ccb55f698c4087231bcd1
amatsegor <macegorarthur@gmail.com>
2018-09-19 10:48:13 +0300

    Sample config file moved

--- lokalize-nodejs/src/main/kotlin/lokalize/index.kt
+++ lokalize-nodejs/src/main/kotlin/lokalize/index.kt
@@ -1,28 +1,3 @@
 package lokalize
 
-import kotlinx.coroutines.experimental.GlobalScope
-import kotlinx.coroutines.experimental.launch
-import lokalize.loader.GoogleSheetLoader
-import lokalize.models.Options
-
-fun main(args: Array<String>) {
-
-    val options = Options("KEY", "android")
-
-    GlobalScope.launch {
-        Lokalize.using(GoogleSheetLoader("1rVIuMUuuJcZNLmAnCRosxOqiZJ-jtRqBz2rkDXvFG8w", arrayOf("Sheet1")))
-                .load(options)
-//                .extract("EN", "output/values/strings.json")
-//                .extract("ES", "output/values-es/strings.json")
-//                .extract("BG", "output/values-bg/strings.json")
-//                .extract("LT", "output/values-lt/strings.json")
-//                .extract("LV", "output/values-lv/strings.json")
-                .extract("EN", "output/values/strings.xml")
-                .extract("ES", "output/values-es/strings.xml")
-                .extract("BG", "output/values-bg/strings.xml")
-                .extract("LT", "output/values-lt/strings.xml")
-                .extract("LV", "output/values-lv/strings.xml")
-                .transform()
-                .save()
-    }
-}
+fun main(args: Array<String>) {  }
